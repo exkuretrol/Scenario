684f4adb052f7ae74bcd7cb8fe15cd6373246982
kuaz <08170875@me.mcu.edu.tw>
2020-04-07 04:09:26 +0800

    fixed Heroku deployment

--- scenario/settings.py
+++ scenario/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello.apps.HelloConfig',
-    'todo',
+    'todo.apps.TodoConfig',
     'pages.apps.PagesConfig'
 ]
 
--- scenario/urls.py
+++ scenario/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello.views import myView
 from todo.views import todoView, addTodo, deleteTodo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sayHello/', myView),
     path('todo/', todoView),
     path('addTodo/', addTodo),
     path('deleteTodo/<int:todo_id>/', deleteTodo),
